803535ef93aabcf01ea2ce65221080ff7d98e15c
StarTux <ths.loy@googlemail.com>
2018-10-21 15:50:23 +0200

    Respect index name

--- src/main/java/com/winthier/sql/SQLTable.java
+++ src/main/java/com/winthier/sql/SQLTable.java
@@ -96,6 +96,7 @@ public final class SQLTable<E> {
                 for (Index index: indexes) {
                     counter += 1;
                     String name = "key_" + getTableName() + "_" + counter;
+                    if (index.name() != null) name = index.name();
                     List<SQLColumn> indexColumns = new ArrayList<>();
                     for (String columnName: index.columnList().split(", ?")) {
                         SQLColumn column = getColumn(columnName);
